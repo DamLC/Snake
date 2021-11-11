96477d3bdac33a9c5f1e9450faf35b81127427bd
DamLC <dlc65@free.fr>
2021-11-11 12:09:47 +0100

    function who create the snake and draw his body

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,39 +1,68 @@
 window.onload = function()
 {
-    var canvas;
-    x = 0;
-    y = 0;
+    // notre serpent
+    var skaa;
 
+    var canvas;
+    
     var ctx;
 
-    var delay = 1000;
+    //delai pour le rafraichissement
+    var delay = 100;
     
-
     // init();
 
     function init()
     {
-        /* crée le canvas du jeu */
+        // create the canvas
         canvas = document.createElement('canvas');
         canvas.width = 900;
         canvas.height = 600;
         canvas.style.border = "1px solid";
         document.body.appendChild(canvas);
         
-        /*cree le context */
+        /*create context */
         ctx = canvas.getContext('2d');
 
+        //create snake object
+        skaa = new snake([6,4],[5,4],[4,4]);
         refreshCanvas();
     }
     
     function refreshCanvas()
-    {     
-        x +=2;
-        y +=2;  
-        ctx.fillStyle = '##ff0000';
+    {                 
         ctx.clearRect(0,0,canvas.width,canvas.height);
-        ctx.fillRect(x,y,100,50);
+        skaa.draw();
         setTimeout(refreshCanvas,delay);
+    }
+
+    //draw one block of the body
+    function drawBlock(ctx, position)
+    {
+        var x = position[0] * blockSize;
+        var y = position[1] * blockSize;
+        ctx.fillRect(x, y, blockSize, blockSize);
+    }
+
+    // create the snake body
+    function snake(body)
+    {
+        this.body = body;
+        this.draw = function()
+        {
+            ctx.save();
+            ctx.fillStyle = '##ff0000';
+            for(var i = 0; i < this.body.lenght; i++)
+            {
+                drawBlock(ctx, this.body[i]);
+            }
+            ctx.restore();
+        };
+    }
+
+    function forward()
+    {
+
     }
     
 }
