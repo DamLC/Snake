d1cf39f5bbae18d758200f8b0a0c6066bab7d210
DamLC <dlc65@free.fr>
2021-11-12 11:58:44 +0100

    add function for advanced

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,38 +1,41 @@
 window.onload = function()
 {
-    // notre serpent
+    // our snake
     var skaa;
 
     var canvas;
-    
+    var canvasWidth = 900;
+    var canvasHeight = 600;
+    var blockSize = 30;
     var ctx;
 
-    //delai pour le rafraichissement
+    //delai for the refresh
     var delay = 100;
     
-    // init();
+     init();
 
     function init()
     {
         // create the canvas
         canvas = document.createElement('canvas');
-        canvas.width = 900;
-        canvas.height = 600;
+        canvas.width = canvasWidth;
+        canvas.height = canvasHeight;
         canvas.style.border = "1px solid";
         document.body.appendChild(canvas);
         
-        /*create context */
+        //create context 
         ctx = canvas.getContext('2d');
 
         //create snake object
-        skaa = new snake([6,4],[5,4],[4,4]);
+        skaa = new snake([[6,4],[5,4],[4,4]]);
         refreshCanvas();
     }
     
     function refreshCanvas()
     {                 
         ctx.clearRect(0,0,canvas.width,canvas.height);
-        skaa.draw();
+        skaa.draw();    
+        skaa.forward();    
         setTimeout(refreshCanvas,delay);
     }
 
@@ -51,18 +54,23 @@ window.onload = function()
         this.draw = function()
         {
             ctx.save();
-            ctx.fillStyle = '##ff0000';
-            for(var i = 0; i < this.body.lenght; i++)
+            ctx.fillStyle = "#ff0000";
+            for(var i = 0; i < this.body.length; i++)
             {
+               
                 drawBlock(ctx, this.body[i]);
             }
-            ctx.restore();
+            ctx.restore(); 
         };
+        //move the snake
+        this.forward = function()
+        {
+            var nextPosition = this.body[0].slice();
+            nextPosition[0]++;
+            this.body.unshift(nextPosition); // add the new position on the array [7,4]
+            this.body.pop(); // delete the last position [4,4]
+
+        }
     }
 
-    function forward()
-    {
-
-    }
-    
 }
